f3b17a3ec1458f1a3ef2411c54b86262f4f1416e
Matt Driscoll <mdriscoll@esri.com>
2022-02-24 11:23:56 -0800

    cleanup

--- demos/2-custom-class/app/LayerFX.ts
+++
@@ -1,9 +0,0 @@
-import Accessor from "esri/core/Accessor";
-import { property, subclass } from "esri/core/accessorSupport/decorators";
-
-@subclass("esri.demo.LayerFX")
-class LayerFX extends Accessor {
-  // simple class
-}
-
-export = LayerFX;
