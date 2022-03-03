eea186442fd4f7b56648249cf9c5971c50960dbd
ryan0000 <rlibed@esri.com>
2022-03-03 15:31:31 -0800

    updated steps

--- demos/bootstrap-start/src/index.tsx
+++ demos/bootstrap-start/src/index.tsx
@@ -1,8 +1,8 @@
 // STEP 1
-import "bootstrap/dist/css/bootstrap.min.css";
-import $ from "jquery";
-import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import $ from "jquery";
+// import Popper from "popper.js";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
